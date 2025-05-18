0fd08fddcb4566051b23801e9d0390dd0023b0d5
Abel Flores <calamardoaf@hotmail.com>
2025-05-18 00:37:31 -0300

    Finalizacion del proyecto de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,101 @@
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import { db } from "./data/db"
 
 
 
 function App(){
 
-    const [data, setData] = useState(db)//* el valor inicial de este useState es db(es nuestra base de datos)
-    const [cart, setCart] = useState([])//* Cambia el valor de cart a un array vacio */
+
+  
+
+
+
+
+   const initialCart = () => {
+      const localStorageCart= localStorage.getItem('cart')
+      return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
+
+    const [data] = useState(db)//* el valor inicial de este useState es db(es nuestra base de datos)
+    const [cart, setCart] = useState(initialCart)//* Cambia el valor de cart a un array vacio */
+
+
+    const MAX_ITEMS= 5;
+    const MIN_ITEMS=1;
+
+    useEffect(()=>{
+      localStorage.setItem('cart',JSON.stringify(cart))
+    },[cart])
+
+   
 
    function addToCart(item){
       const itemExists= cart.findIndex((guitar)=>guitar.id ===item.id)
-      if(itemExists >=0){ //existe en el carrito
-        item.quantity= item.quantity+1;
-        
+      if(itemExists >=0){// si existe en el carrito entonces 
+        if(cart[itemExists].quantity>=MAX_ITEMS) return;
+        const updateCart = [...cart]
+        updateCart[itemExists].quantity++; 
+        setCart(updateCart)
+
 
       }else{
         console.log("agregando nuevo carrito")
         item.quantity=1 // estoy agregando una nueva propiedad quantity al objeto item(guitar)
-        setCart(prevCart=>[...prevCart,item]) // lo guardo en mi useState cart(carrito)
+        setCart([...cart,item]) // lo guardo en mi useState cart(carrito)
     
         
       }
         
     }
+
+    function removeFromCart(id){
+
+      setCart(prevCart => prevCart.filter(guitar=>guitar.id!== id))
+
+    }
+
+    function increaseQuantity(id){
+      const updatedCart = cart.map(item=>{
+        if(item.id=== id && item.quantity<MAX_ITEMS){
+          return{
+            ...item, // hago una copia de mi guitarra 
+            quantity: item.quantity + 1 // luego le digo a js que modifique quantity sobre esa copia  pd: para esta instruccion siempre debo usar spread opartar (...)
+          }   
+        }
+         return item
+
+      })
+      
+      setCart(updatedCart)
+    }
+
+    function decreaseQuantity(id){
+
+      console.log('decrementando' + id)
+      const updateCart = cart.map(item=>{
+        if(item.id===id && item.quantity > MIN_ITEMS){
+          return{
+            ...item,
+            quantity: item.quantity - 1
+          }  
+        }
+        
+        return item
+      })
+
+      setCart(updateCart)
+      
+    }
+
+    function clearCart(){
+      setCart([])
+
+    }
+
+
+  
 
      
 
@@ -33,7 +104,14 @@ function App(){
   return (
     <>
       
-    <Header/>
+    <Header
+      cart={cart}
+      removeFromCart={removeFromCart}
+      increaseQuantity={increaseQuantity}
+      decreaseQuantity={decreaseQuantity}
+      clearCart={clearCart}
+  
+    />
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
